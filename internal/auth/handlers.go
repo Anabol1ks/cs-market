d3e53ac01b2a2e0bfd0e6643a1f81a10b282ec3f
Anabol1ks <grigorogannisyan.12@gmail.com>
2025-01-21 11:15:56 +0300

    time token

--- internal/auth/handlers.go
+++ internal/auth/handlers.go
@@ -124,7 +124,8 @@ var jwtSecretRefresh = []byte(os.Getenv("JWT_KEY_REFRESH"))
 func GenerateTokensJWT(stramID string) (string, string, error) {
 	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"user_id": stramID,
-		"exp":     time.Now().Add(1 * time.Minute).Unix(),
+		// "exp":     time.Now().Add(15 * time.Minute).Unix(),
+		"exp": time.Now().Add(72 * time.Hour).Unix(),
 	})
 
 	accessTokenString, err := accessToken.SignedString(jwtSecret)
